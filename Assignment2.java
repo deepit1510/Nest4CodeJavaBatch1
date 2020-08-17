ba8e92ab03cd772895caf2efac51ab74e19cc0ec
deepit1510 <59756527+deepit1510@users.noreply.github.com>
2020-08-17 23:37:41 +0530

    Create Assignment2.java

---
+++ Assignment2.java
@@ -0,0 +1,33 @@
+package nest4;
+
+import java.util.Scanner;
+
+public class Assignment2 {
+	static Scanner input = new Scanner("System.in");
+	static float heightConvertor(int hgt, int height) {
+		float f_hgt = (float)  (hgt *12*2.54+ height*2.54);
+		
+		return (float) f_hgt;
+		
+	}
+	
+	static float weightConvertor(int weight) {
+		float f_weight = (float) 2.3 *weight;
+		
+		return (float) f_weight;
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Enter the height feet part");
+		int feet = input.nextInt();
+		System.out.println("Enter the height inch part");
+		int inch = input.nextInt();
+		System.out.println("Enter the weight in kg");
+		int weight = input.nextInt();
+		float result = heightConvertor(feet,inch);
+		float w_result = weightConvertor(weight);
+		System.out.println("Result height in cm = "+result);
+		System.out.println("Result weight in pound = "+w_result);
+	}
+}
