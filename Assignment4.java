8729d3a00588fafac74044b376c0309b305f3c03
deepit1510 <59756527+deepit1510@users.noreply.github.com>
2020-08-19 22:29:35 +0530

    Create Assignment4.java

---
+++ Assignment4.java
@@ -0,0 +1,26 @@
+package nest4;
+
+import java.util.Scanner;
+
+public class restaurant {
+	static Scanner input = new Scanner(System.in);
+		static int a;
+		static int even;
+		static int odd;
+	public static void main(String[] args) {
+		System.out.println("Enter the number");
+		int number = input.nextInt();
+		while (number!=0) {
+			a = number%10;
+			if(a%2 ==0){
+				even +=1;
+			}
+			else {
+				odd +=1;
+			}
+			number = number/10;
+		}
+		System.out.println("Number of Even = "+even);
+		System.out.println("Number of Odd ="+odd);
+	}
+}
