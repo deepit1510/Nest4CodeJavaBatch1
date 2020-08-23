67fa3c27a57729ca3d1ec2d44e864786be2bd8d4
deepit1510 <59756527+deepit1510@users.noreply.github.com>
2020-08-23 19:43:44 +0530

    Create Assignement6-2.java

---
+++ Assignement6-2.java
@@ -0,0 +1,16 @@
+public class CreateHuman {
+	
+	public static void main(String[] args) {
+		
+		humanObj1.age = 22;
+		humanObj1.height = 5;
+		humanObj1.weight = 50;
+		humanObj1.eyes = "blue";
+		humanObj1.hair = "black";
+		humanObj1.walk();
+		humanObj1.display(); */
+		Human humanObj2 = new Human(6, 60,25);
+		humanObj2.display();
+	}
+
+}
