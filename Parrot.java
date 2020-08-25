3b7c5b8c640588283f6f36657b55aceaa37b61cb
deepit1510 <59756527+deepit1510@users.noreply.github.com>
2020-08-26 00:10:50 +0530

    Update Parrot.java

--- Parrot.java
+++ Parrot.java
@@ -3,5 +3,9 @@ public class Pearrot extends Bird implement SwimBehaviour {
 	public void sound() {
 		System.out.println("gurgles and trills...");
 	}
+  	
+	public void swim() {
+		System.out.println("Can not Swim...");
+	}
   
 }
