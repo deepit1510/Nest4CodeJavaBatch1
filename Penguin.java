4706c6962deb269ea788713c370e7a3c21532892
deepit1510 <59756527+deepit1510@users.noreply.github.com>
2020-08-26 00:11:22 +0530

    Update Penguin.java

--- Penguin.java
+++ Penguin.java
@@ -3,4 +3,8 @@ public class Penguin extends Bird implement SwimBehaviour {
 	public sound sound() {
 		System.out.println("honking...");
 	}
+	
+	public void swim() {
+		System.out.println("Can not Swim...");
+	}
 }
